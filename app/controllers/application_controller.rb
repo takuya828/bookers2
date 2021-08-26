dea53bfd9ae36170b42d8c6df17673eee126fb05
takuya828 <takuya11070221@gmail.com>
2021-08-26 13:50:30 +0000

    [add views,bootstrap]4-8

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,9 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 
+
+  def after_sign_in_path_for(resource)
+     user_path(resource)
+  end
+
 end
