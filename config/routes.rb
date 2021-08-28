b13db1279cdfb3f55ee49d082fb6426dff30cdbc
takuya828 <takuya11070221@gmail.com>
2021-08-28 14:11:32 +0000

    [投稿できない、色々修正]4-8

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :books
   root to: 'homes#top'
   get 'about' => 'homes#about', as:'about'
-  resources :users, only: [:show]
+  resources :users, only: [:show, :edit, :update, :index, :new, :create]
+  resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_23_133647) do
+ActiveRecord::Schema.define(version: 2021_08_28_132316) do
 
   create_table "books", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2021_08_23_133647) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.integer "title"
     t.index ["email"], name: "index_books_on_email", unique: true
     t.index ["reset_password_token"], name: "index_books_on_reset_password_token", unique: true
   end
@@ -33,6 +35,8 @@ ActiveRecord::Schema.define(version: 2021_08_23_133647) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.string "profile_image_id"
+    t.text "Introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
